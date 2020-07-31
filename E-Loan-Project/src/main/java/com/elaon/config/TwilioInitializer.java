209a6463e797b4c8f186304abfa14eddb462e3bf
Bayvao Verma <34921943+Bayvao@users.noreply.github.com>
2020-07-31 22:25:32 +0530

    Update TwilioInitializer.java

--- E-Loan-Project/src/main/java/com/elaon/config/TwilioInitializer.java
+++ E-Loan-Project/src/main/java/com/elaon/config/TwilioInitializer.java
@@ -11,13 +11,13 @@ public class TwilioInitializer {
 
 	private final static Logger LOGGER = LoggerFactory.getLogger(TwilioInitializer.class);
 
-	public static final String ACCOUNT_SID = "AC44bf2f58c126b8c3ae50fbd2503aeb3f";
-	public static final String AUTH_TOKEN = "9f7c9ed94ce9d675da5183f446baa983";
-	public static final String TRIAL_NUMBER = "+12098463619";
+	public static final String ACCOUNT_SID = "your_account_sid";
+	public static final String AUTH_TOKEN = "your_account_token";
+	public static final String TRIAL_NUMBER = "your_trail_number";
 
 	public TwilioInitializer() {
 		Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 		
-		LOGGER.info("Twilio Initialized ... congo! " + ACCOUNT_SID);
+		LOGGER.info("Twilio Initialized..." + ACCOUNT_SID);
 	}
 }
